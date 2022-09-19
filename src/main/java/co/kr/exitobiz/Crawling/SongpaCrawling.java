d66532444a407ba25ffb33164f7a384068ec8c34
lee-goeun <koeun1120@daum.net>
2022-09-19 15:36:50 +0900

    송파구일자리통합지원센터 기관 추가

---
+++ src/main/java/co/kr/exitobiz/Crawling/SongpaCrawling.java
@@ -0,0 +1,136 @@
+package co.kr.exitobiz.Crawling;
+
+import co.kr.exitobiz.Mappers.Api.CrawlingMapper;
+import co.kr.exitobiz.Vo.Cms.SupportVo;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Component
+public class SongpaCrawling implements Crawling {
+
+    @Autowired
+    CrawlingMapper crawlingMapper;
+
+    @Autowired
+    Environment environment;
+
+    /*
+     *
+     * 송파구일자리통합지원센터
+     * https://www.songpa.go.kr/
+     *  */
+
+    private String url = "https://www.songpa.go.kr/www/selectBbsNttList.do?bbsNo=92&key=2775&pageIndex=";
+    private int page = 1;
+
+    @Override
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    @Override
+    public void craw() throws InterruptedException {
+
+        String driverPath = environment.getProperty("chrome.driver.path");
+        File driverFile = new File(String.valueOf(driverPath));
+
+        if (!driverFile.exists() && driverFile.isFile()) {
+            throw new RuntimeException("Not found");
+        }
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("no-sandbox");
+        options.addArguments("disable-dev-shm-usage");
+
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(driverFile)
+                //.usingPort(5000)
+                .usingAnyFreePort()
+                .build();
+
+        try {
+            service.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        WebDriver driver = new ChromeDriver(service,options);
+
+        SupportVo supportVo = new SupportVo();
+        supportVo.setTitle("송파구일자리통합지원센터");
+        supportVo.setUrl("https://www.songpa.go.kr/");
+        supportVo.setLocCode("C02");
+        supportVo.setActiveYn("Y");
+        supportVo.setErrorYn("N");
+        List<SupportVo> supportVos = new ArrayList<>();
+
+
+
+        for (int i=page; i>0; i--) {
+            driver.get(url + i);
+            Thread.sleep(1000);
+
+            for(int j=1; j<=10; j++) {
+                    try {
+                        WebElement titleXpath = driver.findElement(By.xpath(" //*[@id=\"board\"]/div/table/tbody/tr[" + j + "]/td[2]/a"));
+                        SupportVo vo = new SupportVo();
+                        String bodyurl = titleXpath.getAttribute("href");
+                        String title = titleXpath.getText();
+
+                        vo.setTargetName("송파구일자리통합지원센터");
+                        vo.setTargetCatName("-");
+                        vo.setLocCode("C02");
+                        vo.setSiTitle(title);
+                        vo.setMobileUrl(bodyurl);
+                        vo.setPcUrl("-");
+
+                        HashMap<String, String> params = new HashMap<>();
+                        params.put("bodyurl", bodyurl);
+                        boolean isUrl = crawlingMapper.isUrl(params);
+                        if (!isUrl) {
+                            supportVos.add(vo);
+                        }
+
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        supportVo.setErrorYn("Y");
+                    }
+            }
+
+            Thread.sleep(500);
+        }
+
+        /* 빈 리스트가 아니면 크레이트 */
+        if (!supportVos.isEmpty()) {
+            try{
+                crawlingMapper.create(supportVos);
+                crawlingMapper.createMaster(supportVo);
+            }catch (Exception e){
+                supportVo.setErrorYn("Y");
+                crawlingMapper.createMaster(supportVo);
+            }
+        }else {
+            supportVo.setErrorYn("N");
+            crawlingMapper.createMaster(supportVo);
+        }
+
+        driver.close();
+        driver.quit();
+        service.stop();
+    }
+
+
+}
