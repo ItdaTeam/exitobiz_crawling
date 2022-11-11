5f15f527818ddb95826399c011102133176ecc74
lee-goeun <koeun1120@daum.net>
2022-11-11 15:11:27 +0900

    mapping url 변경

--- src/main/java/co/kr/exitobiz/Controller/WebApp/MainpageController.java
+++ src/main/java/co/kr/exitobiz/Controller/WebApp/MainpageController.java
@@ -12,13 +12,13 @@ import java.util.HashMap;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/mobile")
+@RequestMapping("/mainpage")
 public class MainpageController {
 
     private final MainpageService mainpageService;
 
     // 인기 키워드 리스트
-    @PostMapping("/mainpage/getSearchHotKeyWord")
+    @PostMapping("/getSearchHotKeyWord")
     @ResponseBody
     public String getSearchHotKeyWord() throws ParseException, JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
@@ -27,7 +27,7 @@ public class MainpageController {
     }
 
     // 나의 최근 키워드 리스트
-    @PostMapping("/mainpage/getMyRecentKeyword")
+    @PostMapping("/getMyRecentKeyword")
     @ResponseBody
     public String getMyRecentKeyword(@RequestHeader HashMap<String, Object> header) throws ParseException, JsonProcessingException {
         HashMap<String, Object> params = new HashMap<>();
@@ -39,14 +39,14 @@ public class MainpageController {
     }
 
     // 나의 최근 키워드 단일 삭제
-    @PostMapping("/mainpage/delMyRecentKeyword")
+    @PostMapping("/delMyRecentKeyword")
     @ResponseBody
     public void delMyRecentKeyword(@RequestBody HashMap<String, Object> body) throws ParseException, JsonProcessingException {
         mainpageService.delMyRecentKeyword(body);
     }
 
     // 나의 최근 키워드 전체 삭제
-    @PostMapping("/mainpage/delMyAllRecentKeyword")
+    @PostMapping("/delMyAllRecentKeyword")
     @ResponseBody
     public void delMyAllRecentKeyword(@RequestHeader HashMap<String, Object> header) throws ParseException, JsonProcessingException {
         HashMap<String, Object> params = new HashMap<>();
@@ -56,7 +56,7 @@ public class MainpageController {
     }
 
     // 타임라인 추가 (나의 최근 검색어 저장) _ 공통
-    @PostMapping("/mainpage/insertTimeLine")
+    @PostMapping("/insertTimeLine")
     @ResponseBody
     public void insertTimeLine(@RequestHeader HashMap<String, Object> header, @RequestBody HashMap<String, Object> body) throws ParseException, JsonProcessingException {
         HashMap<String, Object> params = new HashMap<>();
@@ -75,7 +75,7 @@ public class MainpageController {
 
 
     //배너리스트
-    @PostMapping("/mainpage/getBannerList")
+    @PostMapping("/getBannerList")
     @ResponseBody
     public String getCommunityList() throws ParseException, JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
@@ -84,7 +84,7 @@ public class MainpageController {
     }
 
     // 누적갯수(누적 지원사업 개수, 이번주 지원사업, 정보 제공기관, 누적 가입 기업) 리스트
-    @PostMapping("/mainpage/getTotalCount")
+    @PostMapping("/getTotalCount")
     @ResponseBody
     public String getTotalCount() throws ParseException, JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
@@ -94,7 +94,7 @@ public class MainpageController {
 
 
     // 키워드 정기배송 리스트 (","로 구분)
-    @PostMapping("/mainpage/getKeyword")
+    @PostMapping("/getKeyword")
     @ResponseBody
     public String getKeyword(@RequestHeader HashMap<String, Object> header) throws ParseException, JsonProcessingException {
         HashMap<String, Object> params = new HashMap<>();
@@ -107,7 +107,7 @@ public class MainpageController {
     }
 
     // 키워드 정기배송 추가
-    @PostMapping("/mainpage/insertKeyword")
+    @PostMapping("/insertKeyword")
     @ResponseBody
     public void getKeyword(@RequestHeader HashMap<String, Object> header, @RequestBody HashMap<String, Object> body) throws ParseException, JsonProcessingException {
         body.put("user_id", header.get("user_id"));
@@ -115,7 +115,7 @@ public class MainpageController {
     }
 
     // 키워드 정기배송 단일 삭제
-    @PostMapping("/mainpage/delKeyword")
+    @PostMapping("/delKeyword")
     @ResponseBody
     public void delKeyword(@RequestHeader HashMap<String, Object> header, @RequestBody HashMap<String, Object> body) throws ParseException, JsonProcessingException {
         body.put("user_id", header.get("user_id"));
@@ -123,7 +123,7 @@ public class MainpageController {
     }
 
     // 키워드 정기배송 전체 삭제
-    @PostMapping("/mainpage/delAllKeyword")
+    @PostMapping("/delAllKeyword")
     @ResponseBody
     public void delAllKeyword(@RequestHeader HashMap<String, Object> header) throws ParseException, JsonProcessingException {
         HashMap<String, Object> params = new HashMap<>();
@@ -133,7 +133,7 @@ public class MainpageController {
 
 
     // 엑시토 추천 리스트
-    @PostMapping("/mainpage/getPushBookList")
+    @PostMapping("/getPushBookList")
     @ResponseBody
     public String getPushBookList(@RequestHeader HashMap<String, Object> header) throws ParseException, JsonProcessingException {
         HashMap<String, Object> params = new HashMap<>();
