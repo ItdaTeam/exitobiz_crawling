f069c4b0b2b70ddb9df8bd7fd1aa126e5f8a65d4
TaeUk <bluelizos@gmail.com>
2022-12-05 18:07:03 +0900

    API 수정

--- src/main/java/co/kr/exitobiz/Config/WebMvcConfig.java
+++ src/main/java/co/kr/exitobiz/Config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package co.kr.exitobiz.Config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,4 +18,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/allMember")
                 .addPathPatterns("/cms/main");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://175.126.123.207:3000" ) // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
 }
+
+
