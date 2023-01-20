c296b0661e29460bf42ec265bd490f10a076a782
lee-goeun <koeun1120@daum.net>
2023-01-20 12:08:02 +0900

    수정

--- src/main/java/co/kr/exitobiz/Controller/Api/PushController.java
+++ src/main/java/co/kr/exitobiz/Controller/Api/PushController.java
@@ -2,6 +2,7 @@ package co.kr.exitobiz.Controller.Api;
 
 import co.kr.exitobiz.Service.Api.PushMultipleService;
 import co.kr.exitobiz.Service.Api.PushService;
+import co.kr.exitobiz.Service.Cms.BannerService;
 import co.kr.exitobiz.Service.WebApp.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -29,6 +30,9 @@ public class PushController {
 
     @Autowired
     Environment environment;
+
+    @Autowired
+    BannerService bannerService;
 
     /**
      * DB에서 사용자 토큰을 가져와서 각 사용자별로 FCM Push 발송처리
@@ -76,7 +80,13 @@ public class PushController {
             params2.put("userlocs", userlocs);
         }
 
-        List<Map<String, Object>> userTokens = userService.getUserTokenByLocation(params2);
+        List<Map<String, Object>> userTokens = new ArrayList<>();
+
+        Map<String,Object> usertoken = new HashMap<String,Object>();
+        usertoken.put("usertoken", "e9lDCbfGTl-WF8isL10xi0:APA91bHE-A68f4sHnI10vqLnLRUjXc8l2Caw129D6HW0J-L8gNVEirrrIAlUfCkTxWHbDPdl3gzFQXP9GzkLB9ASyBYO_GbgzcGulu4NPfm17sJFh8nxTgbLf3ah5J-vDKYWQxkjI0Kx");
+
+        userTokens.add(usertoken);
+                //userService.getUserTokenByLocation(params2);
 
         String result ="fail";
         int failCount = 0;
@@ -139,12 +149,20 @@ public class PushController {
      */
     @RequestMapping(value="/sendOne")
     @ResponseBody
-    public String sendOnePush(@RequestParam HashMap<String,String> params ) throws Exception {
-        
+    public String sendOnePush(@RequestBody HashMap<String,String> params ) throws Exception {
 
         params.put("id",params.get("userId")); // 쿼리 공통 사용을 위한 변수 id(사용자 아이디) 추가
+        if(params.get("keyId").equals("comm") || params.get("keyId").equals("like") || params.get("keyId").equals("reco")){
+            params.put("url" , "https://exitobiz.co.kr/community/communityView/" + params.get("idx"));
+        }
 
         List<Map<String,Object>>  userTokens = userService.getUserTokenByKeyId(params); // DB에서 사용자 토큰 가져오기
+
+//        Map<String,Object> usertoken = new HashMap<String,Object>();
+//        usertoken.put("usertoken", "e9lDCbfGTl-WF8isL10xi0:APA91bHE-A68f4sHnI10vqLnLRUjXc8l2Caw129D6HW0J-L8gNVEirrrIAlUfCkTxWHbDPdl3gzFQXP9GzkLB9ASyBYO_GbgzcGulu4NPfm17sJFh8nxTgbLf3ah5J-vDKYWQxkjI0Kx");
+
+//        userTokens.add(usertoken);
+
         if(!userTokens.isEmpty()){ // 토큰이 존재하는 경우에만 처리
                 params.put("firebaseKeyPath",environment.getProperty("firebase.path.key")); // key파일 path 가져오기
                 pushMultipleService.sendListPush(params, userTokens);
@@ -160,7 +178,33 @@ public class PushController {
         params.put("title",URLDecoder.decode(params.get("title")));
         params.put("body",URLDecoder.decode(params.get("body")));
 
-        List<Map<String, Object>> userTokens = userService.getUserTokens();
+        //banner 조회
+        HashMap<String, Object> bannParams = new HashMap<>();
+        bannParams.put("index", params.get("idx"));
+
+        HashMap<String, Object> bannObj = bannerService.getBanner(bannParams);
+
+        if(params.get("keyId").equals("noti")){
+            params.put("url" , "https://exitobiz.co.kr/notice/noticeView/" + params.get("idx"));
+        }else if(params.get("keyId").equals("cus1")){
+            params.put("url" , "https://exitobiz.co.kr/community/communityView/" + params.get("idx"));
+        }else if(params.get("keyId").equals("bann")){
+            if(bannObj.get("banner_ctg").equals("I") || bannObj.get("banner_ctg").equals("O")){
+                params.put("url", (String) bannObj.get("banner_link"));
+            }else if(bannObj.get("banner_ctg").equals("N")){
+                params.put("url", "https://exitobiz.co.kr/notice/noticeList");
+            }else if(bannObj.get("banner_ctg").equals("ND")){
+                params.put("url", "https://exitobiz.co.kr/notice/noticeList/" + params.get("idx"));
+            }
+        }
+
+        List<Map<String, Object>> userTokens = new ArrayList<>();
+        //userService.getUserTokens();
+
+        Map<String,Object> usertoken = new HashMap<String,Object>();
+        usertoken.put("usertoken", "e9lDCbfGTl-WF8isL10xi0:APA91bHE-A68f4sHnI10vqLnLRUjXc8l2Caw129D6HW0J-L8gNVEirrrIAlUfCkTxWHbDPdl3gzFQXP9GzkLB9ASyBYO_GbgzcGulu4NPfm17sJFh8nxTgbLf3ah5J-vDKYWQxkjI0Kx");
+
+        userTokens.add(usertoken);
 
         String result = "fail";
         if(!userTokens.isEmpty()){
