7cdf5dcb2c4cd29ea4191db23dc73279a2066e4a
lee-goeun <koeun1120@daum.net>
2023-03-08 18:19:12 +0900

    중앙정렬 수정

--- src/main/java/co/kr/exitobiz/Scheduler/IntervalScheduler/PushScheduler.java
+++ src/main/java/co/kr/exitobiz/Scheduler/IntervalScheduler/PushScheduler.java
@@ -191,7 +191,7 @@ public class PushScheduler {
                 userParams.put("userid", email.get("user_id"));
                 userParams.put("id", email.get("user_id"));
 
-//                email.replace("email","mykoreareds@naver.com");
+                email.replace("email","itda.korea@gmail.com");
 
                 // 사용자 검색 조건 조회
                 HashMap<String, Object> userCompanyInfo = (HashMap<String, Object>) userService.getCompanyInfo(userParams);
@@ -242,10 +242,10 @@ public class PushScheduler {
                             SimpleDateFormat sdf = new SimpleDateFormat("MM월dd일");
                             String end_dt = sdf.format(timestamp);
                             supportText +=
-                            "<p><a href=\"" + supStr.get("mobile_url") + "\" style=\"text-decoration:none; color:#797979; display:flex; align-items:center;\">"+
-                             "<strong style=\"font-weight:400; background-color:#30D6C2; font-size:14px; display:flex; color:#fff; border-radius:5px; width:92px; height:32px;align-items:center; justify-content:center;\">" + supStr.get("target_cat_nm") + "</strong>&nbsp;&nbsp;"+
-                             "<b style=\"font-weight:400;\">" + supStr.get("locname") + "</b>&nbsp;&nbsp;/&nbsp;&nbsp;<b style=\"font-weight:400; max-width:65%; white-space: nowrap;text-overflow: ellipsis;overflow:hidden;word-break: break-all; -webkit-box-orient: vertical;\">" + supStr.get("si_title") + "</b>&nbsp;&nbsp;/&nbsp;&nbsp;<b style=\"font-weight:400;\">~" + end_dt+ "</b>"+
-                             "</a></p>";
+                            "<div style=\"margin-bottom: 10px;\"><a href=\"" + supStr.get("mobile_url") + "\" style=\"text-decoration:none; color:#797979; display:flex; align-items:center;\">"+
+                                    "<div style=\"font-weight:400; background-color:#30D6C2; font-size:14px; display:flex;color:#fff; border-radius:5px; width:92px; height:32px;line-height: 32px;\"><strong style=\"margin: 0 auto;font-weight:400;\">" + supStr.get("target_cat_nm") + "</strong></div>&nbsp;&nbsp;"+
+                            "<b style=\"font-weight:400;\">" + supStr.get("locname") + "</b>&nbsp;/&nbsp;<b style=\"font-weight:400;max-width:309px; white-space: nowrap;text-overflow: ellipsis;overflow:hidden;word-break: break-all; -webkit-box-orient: vertical;\">" + supStr.get("si_title") +  "</b>&nbsp;/&nbsp;<b style=\"font-weight:400;\">~" + end_dt+ "</b>"+
+                            "</a></div>";
                         }
                     }else{
                         supportText +=  "<p style=\"color: #797979; margin:20px 0 0; font-weight:400; font-size:14px;\">맞춤 지원사업이 없어요.<a href=\"https://exitobiz.co.kr\" style=\"color:#0000FF;\">기업정보를 수정해보세요.</a></p>";
